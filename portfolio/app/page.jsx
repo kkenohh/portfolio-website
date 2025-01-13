8454041fb9876d8bc6db040522c036445c67e392
Ken <kenoh17@uw.edu>
2025-01-13 03:06:06 -0800

    just really small changes that arent worth noting

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -12,13 +12,10 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br />
+              Hello! I'm <br />
               <span className="text-accent">Ken Oh</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digital experiences and I am
-              proficient in various programming languages and technologies.
-            </p>
+            <p className="max-w-[500px] mb-9 text-white/80"></p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
                 variant="outline"
